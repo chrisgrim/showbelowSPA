2038bb9f1aec38d192124abb480509293aa4476a
Sergey <memonanofsa@gmail.com>
2019-05-17 23:36:33 +0500

    Remove lang from 404

--- resources/js/pages/errors/404.vue
+++ resources/js/pages/errors/404.vue
@@ -1,12 +1,12 @@
 <template>
   <card class="text-center">
     <h3 class="mb-4">
-      {{ $t('page_not_found') }}
+      {{ 'page_not_found' }}
     </h3>
 
     <div class="links">
       <router-link :to="{ name: 'welcome' }">
-        {{ $t('go_home') }}
+        {{ 'go_home' }}
       </router-link>
     </div>
   </card>
